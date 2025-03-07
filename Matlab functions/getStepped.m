6f2f601d4e83105d1d983072a6320899fe75ed6f
Jacob Chalif <jacobchalif@Jacobs-MacBook-Pro-2.local>
2025-03-07 13:52:07 -0500

    first commit

---
+++ Matlab functions/getStepped.m
@@ -0,0 +1,17 @@
+
+
+function [stepX,stepY] = getStepped(minX,maxX,y)
+    [~,si] = sort(minX,'ascend');
+    minX = minX(si);
+    maxX = maxX(si);
+    y = y(si);
+    N = length(y);
+    stepX = nan(N*2,1);
+    stepY = nan(N*2,1);
+    for i = 1:N
+        stepX((i-1)*2+1) = minX(i);
+        stepX((i-1)*2+2) = maxX(i);
+        stepY((i-1)*2+1) = y(i);
+        stepY((i-1)*2+2) = y(i);
+    end
+end
